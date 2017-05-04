2f7ed09d79cd337939b44a69b18563d6ad8a4388
carolabothe <carola.bothe@gmx.de>
2017-05-04 10:09:46 +0000

    error handling final

--- ueb01.c
+++ ueb01.c
@@ -5,27 +5,35 @@ Tuğçe Çetinkaya and Carola Bothe
 
 #include <stdio.h>
 #include <inttypes.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]){
-	if (size(argc)==2){
-		fprintf(stderr,"Please enter at least 2 arguments."); //Programm braucht mind. 2 Argumente um Summe zu bilden
-	}	
-	char* ptr, ptr2;
-	int sum = 0;
-    for (int i = 1; i < argc-1; ++i){	
-    	sum += (int) strtol(argv[i], &ptr, 10);	//atoi alternative:  kein string part, basis ist 10 (dezimal)
-    	
-
-    }
-    char c;
-    if (sum == (int) strtol(argv[argc-1], &ptr2, 10)){
-    	c = '1';
-    }
-    else{
-    	c = '0';
-    }
-    if (size(ptr) != 0) || (size(ptr2) != 0){
-    	fprintf(stderr,"Please enter integers only."); //strtol speichert alles nicht konvertierbare in einem string, der für unser programm also leer sein sollte
+if (argc<=2){
+	fprintf(stderr,"Please enter at least 2 arguments.\n"); //Programm braucht mind. 2 Argumente um Summe zu bilden
 	}
+else{	
+	char *ptr;
+	char *ptr2;
+	int sum = 0;
+    	for (int i = 1; i < argc-1; ++i){	
+    		sum += strtol(argv[i], &ptr, 10);	//atoi alternative:  kein string part, basis ist 10 (dezimal)
+    		if (*ptr != 0){	
+    			fprintf(stderr,"Please enter integers only.\n"); 
+    		}
+    	}
+    	char c;
+    	if (sum == strtol(argv[argc-1], &ptr2, 10)){
+    		c = '1';
+		if (*ptr2 != 0){	
+    			c = '0';
+			fprintf(stderr,"Last argument is not an integer.\n");
+    		}
+		
+    	}
+    	else{
+    		c = '0';
+    	}
+    	
     printf("%c\n",c);
+}
 }

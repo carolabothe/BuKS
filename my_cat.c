e87439d4fe864e1f61a8b23bf3a6f9626324eb8c
carolabothe <carola.bothe@gmx.de>
2017-05-18 16:17:55 +0200

    mein zeug von gerade, ich misch das gleich manuell mit deinem

--- my_cat.c
+++ my_cat.c
@@ -5,17 +5,26 @@
 
 //gibt Inhalt der Datei aus
 int main(int argc, char *argv[]){
-	if (argc != 2){
-		printf("Please enter exactly one argument: the file name.\n");
+	if (argc < 2){
+		printf("Please enter at least one argument to read from: a file name or the text directly.\n");
 		return 1;
 	}
 	char *fname = argv[1];
 	FILE *data=fopen(fname, "r");
 	if (data == NULL){
-     	printf("Cannot open the file.\n");
-      	return 1;
-    }
-    	printf("%s\n",data); //finde das richtig %irgendwas nicht 
+     		printf("Cannot open the file. Trying to read from stdin.\n");
+		printf("%s\n",fname);    
+}
+    	
+	else{
+		fscanf(data, fname);
+		printf(data);
+		printf("\n");} 
 	fclose(data);
+	
+
+
+	
+	
 	return 0;
 }

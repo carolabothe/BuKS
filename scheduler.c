c3c90146f82bb42ff54066aca4e5c19e655df451
Carola Bothe <Carola@CarolasMacBookPro.fritz.box>
2017-05-31 22:57:20 +0200

    rr works

--- scheduler.c
+++ scheduler.c
@@ -25,24 +25,55 @@
 
 //round robin:
 void rr(struct Process* head) {
-	Process* current = head->next;
-	while(current != head){
-		if(current->state == RUNNING){
-			if(current->next == head){	//head ist Dummy und soll nicht dran sein
-				current->next->next->state = RUNNING;
+	uint64_t readycounter = 0;
+	uint64_t runcounter = 0;
+	Process* readyP = head;
+	Process* runningP = head;
+	//bestimmen wie viele programme ready sind und welche und welches läuft, wenn eins läuft
+	for(Process* p = head->prev;p != head;p=p->prev){	//rückwärts damit erster READY Prozess aus Schlange genommen wird
+		if(p->state ==READY){
+			readycounter ++;
+			readyP = p; 	
+		}
+		if(p->state == RUNNING){
+			runcounter ++;
+			runningP = p;
+		}
+	}
+	//setzen Status
+	if(runcounter>1){fprintf(stderr,"Error: Mehrere Programme laufen gleichzeitig!");}
+	if(readycounter == 1){ //nur ein READY Prozess in Schlange
+		if(runcounter==0){
+			readyP->state = RUNNING;
+		}
+		else{
+			if(runningP->cycles_todo == 0){
+				runningP->state = DEAD;
 			}
 			else{
-				current->next->state = RUNNING;
+				runningP->state=READY;
+				readyP->state = RUNNING;	
 			}
-			if(current->cycles_todo == 0){
-				current->state = DEAD;
-			}
-			else {
-				current->state=READY;
-			}
-		}		
-		current = current->next;	
-	}	
+			readyP->state = RUNNING;
+		}
+	}
+	else if(readycounter == 0){	//Programm soll weiter laufen, wenn es noch Zyklen übrig hat
+		if(runningP->cycles_todo == 0){
+			runningP->state = DEAD;
+		}
+		else {
+			runningP->state=RUNNING; //brauchen wir eig nicht da schon der Fall...		
+		}
+	}
+	else{ //mehrere wartende READY Programme
+		if(runningP->cycles_todo == 0){
+			runningP->state = DEAD;
+		}
+		else {
+			runningP->state=READY;	
+		}
+		readyP->state = RUNNING;	//readyP sollte automatisch erstes sein wegen rückläufiger schleife oben
+	}
 }
 
 //first come first serve: prozesse durchgehen und gucken welcher gerade läuft, wenn einer noch auf running ist dann weiterlaufen lassen sonst nach process mit längster wartezeit suchen und den auf running setzen

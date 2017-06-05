6e66d8aa72283f74ffcec9f87256713451003bce
carolabothe <carola.bothe@gmx.de>
2017-06-05 16:05:29 +0200

    hm also irgendwie macht es nichts aber komme gerade nicht weiter

--- my_ls.c
+++ my_ls.c
@@ -4,22 +4,26 @@
 #include <dirent.h>
 
 int main(int argc, char* argv[]){
+//auswählen der Option
+
+
+
 //Auflisten des aktuellen Ordnerinhalts, keine explizite Sortierung gefordert
 //hilfreiche Seite: http://pubs.opengroup.org/onlinepubs/007908775/xsh/readdir.html
-dirp = opendir(".");	//öffnen momentanes Verzeichnis
-while(dirp){
-	readdir(dirp);
-}        
+DIR* dirp = opendir(".");	//öffnen momentanes Verzeichnis
+if(dirp == NULL){printf("Fehler beim Öffnen.");}
+struct dirent* entry = readdir(dirp);
+while((entry = readdir(dirp))){ 
+	printf("%c\t",entry->d_name[FILENAME_MAX]); ////FILENAME_MAX ist in stdio definiert
+}
 closedir(dirp);
-
-//struct dirent *readdir(DIR *dirp);
-//int readdir_r(DIR *dirp, struct dirent *entry, struct dirent **result);
+//printf("\n");
 
 
 
 
 
-// my_ls -l Auflisten des aktuellen Ordnerinhalts, keine explizite Sortierung gefordert
+// my_ls -l Auflisten des aktuellen Ordnerinhalts, keine explizite Sortierung gefordert –>alphasort?
 // my_ls -a Versteckte Dateien und Ordner mit auflisten 
 // my_ls beliebiger/ordner bzw. my_ls beliebige/datei = Auslesen des angegebenen Ordners bzw. der angegebenen Datei
 

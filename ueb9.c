bdb14bb7c759cc1faa1f8289d0204a1420e6d482
Carola Bothe <Carola@CarolasMacBookPro.fritz.box>
2017-06-23 17:46:11 +0200

    erster teil von b läuft

--- ueb9.c
+++ ueb9.c
@@ -1,11 +1,28 @@
 #include<stdio.h> 
-#include<string.h>  
+#include<string.h> 
+#include<stdlib.h> 
 #include<inttypes.h>
 
 char cxor (char a, char b);	//xor für chars
 char* crc(char* bits); //bekommt payload als string und gibt prüfsumme als string zurück
 int main(int argc , char *argv[]){
-	printf("Prüfsumme: %s\n",crc(argv[1]));
+	if (argc == 1){
+		printf("Prüfsumme: %s\n",crc(argv[1]));
+	}
+	else if(argc == 2){
+		FILE * file;
+		file = fopen( argv[1] , "r+");	//open given file; man: "a+" Open for reading and writing.  The file is created if it does not exist.  The stream is positioned at the end of the file.
+		char* idc = malloc(1000);		//idc because i dont care about the name
+		char* payload = fgets(idc,1024,file); //1024 ist einfach mal das limit der stringlänge
+		//printf("Prüfsumme: %s\n",crc(payload));
+		fprintf(file, "%s",crc(payload)); //klappt nicht aber mit 'a+' oben gibt es segmentation fault
+	
+		free(idc);
+	}
+	else{
+		fprintf(stderr,"Falsche Anzahl an Argumenten.\n");
+		return 1;
+	}
 	return 0;
 }
 

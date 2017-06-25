d46778bf19b1d7ada52eb13fc8ef35dd007d4271
Carola Bothe <Carola@CarolasMacBookPro.fritz.box>
2017-06-25 23:16:12 +0200

    fertig hoffe ich

--- client.c
+++ client.c
@@ -28,17 +28,17 @@ int main(int argc , char *argv[]){
 		switch(argv[1][1]){
 			case 'U': 
 				sock = socket(AF_UNIX , SOCK_STREAM , 0);
-				server.sin_family = AF_LOCAL;
+				server.sin_family = AF_UNIX;
 				socktype = 0;
 				break;
 			case 'u': 
 				sock = socket(AF_INET , SOCK_DGRAM , 0);
-				server.sin_family = AF_LOCAL;
+				server.sin_family = AF_INET;
 				socktype = 1;
 				break;
 			case 't': 
 				sock = socket(AF_INET , SOCK_STREAM , 0);
-				server.sin_family = AF_LOCAL;
+				server.sin_family = AF_INET;
 				socktype = 2;
 				break;
 
@@ -49,31 +49,58 @@ int main(int argc , char *argv[]){
     if (sock == -1){
         fprintf(stderr, "Konnte Socket nicht erstellen.\n");
     }
-    printf("Socket wurde erstellt.\n");
-     
+    else{
+    	printf("Socket wurde erstellt.\n");
+    }
+
     server.sin_addr.s_addr = inet_addr(argv[2]); //nimmt das argv[2]
     server.sin_port = htons( 5555 ); // man kann beliebigen Port wählen
-	if((socktype == 0) | (socktype == 2)){ //UNIX 
-    	if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0){	
+	/*if((socktype==1)|(socktype==2)){ //argv[2] gibt IP/port
+    	unsigned char buf[sizeof(struct in_addr)];
+		inet_pton(AF_INET,argv[2],buf);
+		server.sin_port = buf;
+		free(buf);
+	} */
+
+	if((socktype == 0) | (socktype == 2)){ //stream sockets
+    	if (connect(sock , (struct sockaddr *)&server , sizeof(struct sockaddr_in)) < 0){	
     		fprintf(stderr, "Connect hat nicht funktioniert.\n");
     	    return 1;
     	}
     	puts("Connected\n");
+    	while(1){//kommunizieren mit dem server
+        	if( send(sock , argv[3] , strlen(argv[3]) , 0) < 0){ //senden..
+    			fprintf(stderr, "Senden fehlgeschlagen.\n");
+    		    return 1;
+    		}
+        	 
+       		//antwort vom server bekommen
+       		if(recv(sock , server_reply , 2017 , 0) < 0){
+       		    fprintf(stderr,"keine Antwort.\n");
+       		    break;
+       		}
+         
+       		puts("Server reply :");
+       		puts(server_reply);
+   		}
     }
     else if (socktype == 1){ //datagram socket
-			printf("%s: unbekannter Host '%s' \n"); 
-			}//sendto 
-			
+        	if(sendto(sock , argv[3] , strlen(argv[3]) , 0,
+        	 (struct sockaddr*)&server,sizeof(struct sockaddr_in)) < 0){ //senden..
+    			fprintf(stderr, "Senden fehlgeschlagen.\n");
+    		    return 1;
+    		}			
         	//antwort vom server bekommen
-        	if( recv(sock , server_reply , 2017 , 0) < 0){
+        	if(recvfrom(sock, server_reply, 2017,0,NULL,NULL)<0){//wissen, wer sendet
     			fprintf(stderr, "Keine Antwort.");
         	}
 			
         	puts("Server reply :");
         	puts(server_reply);	
 
-    
+    }
     shutdown(sock,2); //schließt den socket
-    return 0;
+    fflush(stdout); // fals kein Zeilenumbruch gesendet wurde
+	return 0;
 	}
 }

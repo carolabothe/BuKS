81c5e93d984de0410e0a4b689bc40747ce843e9a
Carola Bothe <Carola@CarolasMacBookPro.fritz.box>
2017-06-26 00:01:28 +0200

    server läuft also der tcp teil

--- server.c
+++ server.c
@@ -26,17 +26,17 @@ int main(int argc , char *argv[]){
 		switch(argv[1][1]){
 			case 'U': 
 				sock = socket(AF_UNIX , SOCK_STREAM , 0);
-				server.sin_family = AF_LOCAL;
+				server.sin_family = AF_UNIX;
 				socktype = 0;
 				break;
 			case 'u': 
 				sock = socket(AF_INET , SOCK_DGRAM , 0);
-				server.sin_family = AF_LOCAL;
+				server.sin_family = AF_INET;
 				socktype = 1;
 				break;
 			case 't': 
 				sock = socket(AF_INET , SOCK_STREAM , 0);
-				server.sin_family = AF_LOCAL;
+				server.sin_family = AF_INET;
 				socktype = 2;
 				break;
 
@@ -44,53 +44,63 @@ int main(int argc , char *argv[]){
 				fprintf(stderr, "Keine gültige Option.\n");
 				return 2;
 		}
-		if (sock == -1 || sock >0){
-			fprintf(stderr, "Konnte Socket nicht erstellen");
+		if (sock == -1){
+			fprintf(stderr, "Konnte Socket nicht erstellen\n");
 			exit(1);
+		}
     }
-    printf("Socket wurde erstellt");
+    printf("Socket wurde erstellt\n");
      
     //wichtig für die socket Adresse in struct
     server.sin_family = AF_INET;
-    server.sin_addr.s_addr = inet_addr(argv[2]);
-    server.sin_port = htons( 5555 );
+    server.sin_addr.s_addr = htonl(INADDR_ANY); //inet_addr(argv[2]); geht nicht
+    server.sin_port = htons( 5555 ); //legen wir fest
      
     //Adresse an den Server binden
-    if( bind(socket_descriptor,(struct sockaddr *)&server , sizeof(server)) < 0){
+    if( bind(sock,(struct sockaddr *)&server , sizeof(server)) < 0){
         //Schickt die error message an sdtrr
         perror("bind fehlgeschlagen");
         return 1;
     }
     puts("bind geklappt");
-     
+
     //Listen
-    listen(socket_descriptor , 3);
+    listen(sock, 3);
+    while(1){
+    if((socktype==0)|(socktype==2)){ //tcp Fall
+    	//Falls noch keine Eingabe da ist
+    	puts("Warte auf Eingabe vom Client...");
+    	c = sizeof(struct sockaddr_in);
      
-    //Falls noch keine Eingabe da ist
-    puts("Warte auf Eingabe vom Client...");
-    c = sizeof(struct sockaddr_in);
+    	//accept die Eingabe vom client
+    	client_sock = accept(sock, (struct sockaddr *)&client, (socklen_t*)&c);
+    	if (client_sock < 0){
+    	    perror("Eingabe hat nicht geklappt\n");
+    	    return 1;
+    	}
+    	puts("Eingabe hat geklappt");
      
-    //accept die Eingabe vom client
-    client_sock = accept(socket_descriptor, (struct sockaddr *)&client, (socklen_t*)&c);
-    if (client_sock < 0){
-        perror("Eingabe hat nicht geklappt");
-        return 1;
+    	while( (read_size = recv(client_sock , client_message , 2017 , 0)) > 0 ){		
+			FILE * file;
+			file = fopen(client_message , "r+"); //Öffnen Dateipfad, den uns client schickt
+			char* idc = malloc(1000);		
+			char* content = fgets(idc,1024,file); //1024 ist einfach mal das limit der stringlänge	
+			write(client_sock , content , strlen(content));
+			free(idc);
+     	}
+    	if(read_size == 0){
+    	    puts("Client rausgeflogen"); //der Client disconnected
+    	    fflush(stdout);
+    	}
+    	else if(read_size == -1){
+    	    fprintf(stderr, "übergabe der Nachricht ist fehlgeschlagen\n");
+    	}
     }
-    puts("Eingabe hat geklappt");
-     
-    //vom Client die nachricht bekommen:
-    while( (read_size = recv(client_sock , client_message , 2017 , 0)) > 0 ){
-        write(client_sock , client_message , strlen(client_message));
+    else{
+    //todo: udp
     }
-     
-    if(read_size == 0){
-        puts("Client rausgeflogen"); //der Client disconnected
-        fflush(stdout);
     }
-    else if(read_size == -1){
-        fprintf(stderr, "übergabe der Nachricht ist fehlgeschlagen");
-    }
-     
+    shutdown(sock,2); //schließt den socket
     return 0;
-    }
+
 }
